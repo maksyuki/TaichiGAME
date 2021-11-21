b7681a623087d55189597bfc92ff4e3b0ec7b07f
maksyuki <maksyuki@126.com>
2021-11-21 08:23:59 +0000

    feat: add camera and modify test module

---
+++ tests/test_matrix.py
@@ -0,0 +1,6 @@
+def func(x):
+    return x + 1
+
+
+def test_matrix():
+    assert func(4) == 5
--- tests/test_vector2.py
+++
@@ -1,4 +0,0 @@
-from ..TaichiGAME.math.vector3 import Vector3
-
-Vector3.demo()
-# print(Vect.Config.Pi)
