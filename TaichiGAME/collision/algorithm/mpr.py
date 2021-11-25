80fd5288f8d25c71883fcbaa813a4ada42fbf3ea
maksyuki <maksyuki@126.com>
2021-11-25 06:47:01 +0000

    feat: add mpr helper methods define

---
+++ TaichiGAME/collision/algorithm/mpr.py
@@ -0,0 +1,13 @@
+
+
+class MPR():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def discover(self, shape_a, shape_b):
+        pass
+
+    @staticmethod
+    def refine(self, shape_a, shape_b, src, center_to_origin, iter=50):
+        pass
