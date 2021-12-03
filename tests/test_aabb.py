73e8d4a01a33e86f0b2a3ad6c7dd4e4c5f85be8b
maksyuki <maksyuki@126.com>
2021-12-03 08:56:03 +0000

    test: add dut module import path

---
+++ tests/test_aabb.py
@@ -0,0 +1,10 @@
+
+from typing import List, Dict, Optional, Tuple
+
+import numpy as np
+
+# from TaichiGAME.collision.broad_phase.aabb import AABB
+
+class TestAABB():
+    def test__eq__(self):
+        assert 1
