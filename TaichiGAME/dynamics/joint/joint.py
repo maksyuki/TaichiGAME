9cb8735a1e6aec1054fbcb0936240ddbe4dc7ba5
maksyuki <maksyuki@126.com>
2021-12-05 05:55:25 +0000

    feat: add needed type hint to the joint module

--- TaichiGAME/dynamics/joint/joint.py
+++ TaichiGAME/dynamics/joint/joint.py
@@ -1,4 +1,11 @@
-class JointType():
+from enum import IntEnum, unique
+from typing import List, Dict, Optional, Tuple
+
+import numpy as np
+
+
+@unique
+class JointType(IntEnum):
     Distance = 0
     Point = 1
     Rotation = 2
@@ -11,52 +18,56 @@ class JointType():
 
 class Joint():
     def __init__(self):
-        self._active = True
-        self._type = None
-        self._id = 0
+        self._active: bool = True
+        self._type: Optional[JointType] = None
+        self._id: int = 0
 
-    def prepare(self, dt):
+    def prepare(self, dt: float):
         pass
 
-    def solve_velocity(self, dt):
+    def solve_velocity(self, dt: float):
         pass
 
-    def solve_position(self, dt):
+    def solve_position(self, dt: float):
         pass
-
-    def active(self):
+    
+    @property
+    def active(self) -> bool:
         return self._active
 
-    def set_active(self, active):
+    @active.setter
+    def active(self, active: bool):
         self._active = active
 
-    def type(self):
+    def type(self) -> JointType:
         return self._type
 
-    def id(self):
+    @property
+    def id(self) -> int:
         return self._id
 
-    def set_id(self, id):
+    @id.setter
+    def id(self, id: int):
         self._id = id
 
     @staticmethod
-    def natural_frequency(frequency):
-        return np.pi * frequency
+    def natural_frequency(freq: float) -> float:
+        return 2 * np.pi * freq
 
     @staticmethod
-    def spring_damping_cofficient(mass, natural_freq, damping_radio):
+    def spring_damping_cofficient(mass: float, natural_freq: float, damping_radio: float) -> float:
         return damping_radio * 2.0 * mass * natural_freq
 
     @staticmethod
-    def spring_stiffness(mass, natural_freq):
+    def spring_stiff(mass: float, natural_freq: float) -> float:
         return mass * natural_freq * natural_freq
 
     @staticmethod
-    def constraint_impulse_mixing(dt, stiffness, damping):
-        cim = dt * (dt * stiffness + damping)
+    def constraint_impulse_mixing(dt: float, stiff: float, damping: float) -> float:
+        cim: float = dt * (dt * stiff + damping)
         return 0.0 if np.isclose(cim, 0.0) else 1.0 / cim
 
     @staticmethod
-    def error_reduction_parameter(dt, stiffness, damping):
-        erp = dt * stiffness + damping
-        return 0.0 if np.isclose(erp, 0.0) else stiffness / erp
+    def error_reduction_parameter(dt: float, stiff: float, damping: float) -> float:
+        erp: float = dt * stiff + damping
+        return 0.0 if np.isclose(erp, 0.0) else stiff / erp
