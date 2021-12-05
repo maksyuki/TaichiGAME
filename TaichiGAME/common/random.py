d0478fb8239adefbd6073596d04b7a3d1b9ac2fd
maksyuki <maksyuki@126.com>
2021-12-05 12:02:02 +0000

    feat: add seq id gen methods for the random module

--- TaichiGAME/common/random.py
+++ TaichiGAME/common/random.py
@@ -1,1 +1,20 @@
-import numpy as np
+from typing import List
+
+
+class RandomGenerator():
+    # now only use the seq methods(not random) to give unique id
+    empty_list: List[int] = []
+    start_id: int = 1000
+
+    @staticmethod
+    def unique() -> int:
+        if len(RandomGenerator.empty_list) > 0:
+            res: int = RandomGenerator.empty_list.pop()
+            return res
+
+        RandomGenerator.start_id += 1
+        return RandomGenerator
+
+    @staticmethod
+    def pop(id: int):
+        RandomGenerator.empty_list.append(id)
