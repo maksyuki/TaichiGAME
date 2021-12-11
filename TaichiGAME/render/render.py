c75a7e8f0fc7052e5b6437836dbd51a94b74227f
maksyuki <maksyuki@126.com>
2021-12-11 19:54:00 +0800

    fix: fix some type hints err in render module

--- TaichiGAME/render/render.py
+++ TaichiGAME/render/render.py
@@ -1,21 +1,24 @@
-from TaichiGAME.common.config import Config
-from typing import List, Tuple
+from typing import List, Optional, Tuple, cast
 
 import numpy as np
 
-from taichi.misc.gui import GUI
+try:
+    from taichi.ui.gui import GUI
+except ImportError:
+    from taichi.misc.gui import GUI
 
 from ..math.matrix import Matrix
 from ..geometry.shape import Circle, Edge, Polygon, Shape, ShapePrimitive
 from ..dynamics.joint.joint import Joint
 from ..collision.broad_phase.aabb import AABB
+from ..common.config import Config
 from ..common.camera import Camera
 
 
 class Render():
     @staticmethod
-    def rd_point(gui: GUI, cam: Camera, point: Matrix):
-        assert gui != None and cam != None
+    def rd_point(gui: GUI, cam: Camera, point: Matrix) -> None:
+        assert gui is not None and cam is not None
 
         view_width: float = cam.viewport.width
         view_height: float = cam.viewport.height
@@ -24,15 +27,15 @@ class Render():
         gui.circle([scrnp.x / view_width, scrnp.y / view_height])
 
     @staticmethod
-    def rd_points(gui: GUI, cam: Camera, points: List[Matrix]):
-        assert gui != None and cam != None
+    def rd_points(gui: GUI, cam: Camera, points: List[Matrix]) -> None:
+        assert gui is not None and cam is not None
 
         for p in points:
             Render.rd_point(gui, cam, p)
 
     @staticmethod
-    def rd_line(gui: GUI, cam: Camera, p1: Matrix, p2: Matrix):
-        assert gui != None and cam != None
+    def rd_line(gui: GUI, cam: Camera, p1: Matrix, p2: Matrix) -> None:
+        assert gui is not None and cam is not None
 
         scrnp1: Matrix = cam.world_to_screen(p1)
         scrnp2: Matrix = cam.world_to_screen(p2)
@@ -45,26 +48,28 @@ class Render():
         gui.line([scrnp1.x, scrnp1.y], [scrnp2.x, scrnp2.y])
 
     @staticmethod
-    def rd_lines(gui: GUI, cam: Camera, lines: List[Tuple[Matrix, Matrix]]):
-        assert gui != None and cam != None
+    def rd_lines(gui: GUI, cam: Camera, lines: List[Tuple[Matrix,
+                                                          Matrix]]) -> None:
+        assert gui is not None and cam is not None
 
         for lin in lines:
             Render.rd_line(gui, cam, lin[0], lin[1])
 
     @staticmethod
-    def rd_shape(gui: GUI, cam: Camera, prim: ShapePrimitive):
-        assert gui != None and cam != None
+    def rd_shape(gui: GUI, cam: Camera, prim: ShapePrimitive) -> None:
+        assert gui is not None and cam is not None
+        assert prim._shape is not None
 
-        if prim._shape.type() == Shape.Type.Polygon:
-            #[trick] draw polygon by draw multi triangle
-            poly: Polygon = prim._shape
+        if prim._shape.type == Shape.Type.Polygon:
+            # [trick] draw polygon by draw multi triangle
+            poly: Polygon = cast(Polygon, prim._shape)
             assert len(poly.vertices) >= 3
 
-            outer_line_st: np.ndarray = None
-            outer_line_ed: np.ndarray = None
-            fill_tri_pa: np.ndarray = None
-            fill_tri_pb: np.ndarray = None
-            fill_tri_pc: np.ndarray = None
+            outer_line_st: Optional[np.ndarray] = None
+            outer_line_ed: Optional[np.ndarray] = None
+            fill_tri_pa: Optional[np.ndarray] = None
+            fill_tri_pb: Optional[np.ndarray] = None
+            fill_tri_pc: Optional[np.ndarray] = None
 
             is_first: bool = True
 
@@ -94,6 +99,12 @@ class Render():
                     np.concatenate((outer_line_st, tmpa))
                     np.concatenate((outer_line_ed, tmpb))
 
+            assert outer_line_st is not None
+            assert outer_line_ed is not None
+            assert fill_tri_pa is not None
+            assert fill_tri_pb is not None
+            assert fill_tri_pc is not None
+
             fill_tri_pa = np.repeat(np.array([outer_line_st[0]]),
                                     vert_len - 3,
                                     axis=0)
@@ -109,19 +120,20 @@ class Render():
                           fill_tri_pc,
                           color=Config.FillColor)
 
-        elif prim._shape.type() == Shape.Type.Ellipse:
-            pass
+        elif prim._shape.type == Shape.Type.Ellipse:
+            raise NotImplementedError
 
-        elif prim._shape.type() == Shape.Type.Circle:
-            cir: Circle = prim._shape
+        elif prim._shape.type == Shape.Type.Circle:
+            cir: Circle = cast(Circle, prim._shape)
             scrnp: Matrix = cam.world_to_screen(prim._xform)
             gui.circle([scrnp.x, scrnp.y],
                        radius=cir.radius * cam.meter_to_pixel)
 
-        elif prim._shape.type() == Shape.Type.Curve:
-            pass
-        elif prim._shape.type() == Shape.Type.Edge:
-            edg: Edge = prim._shape
+        elif prim._shape.type == Shape.Type.Curve:
+            raise NotImplementedError
+
+        elif prim._shape.type == Shape.Type.Edge:
+            edg: Edge = cast(Edge, prim._shape)
             Render.rd_point(gui, cam, edg.start + prim._xform)
             Render.rd_point(gui, cam, edg.end + prim._xform)
             Render.rd_line(gui, cam, edg.start + prim._xform,
@@ -129,16 +141,17 @@ class Render():
 
             center: Matrix = (edg.start + edg.end) / 2.0
             center += prim._xform
-            Render.rd_line(gui, cam, center, center + 0.1 * edg.normal)
+            Render.rd_line(gui, cam, center, center + edg.normal * 0.1)
 
-        elif prim._shape.type() == Shape.Type.Capsule:
-            pass
-        elif prim._shape.type() == Shape.Type.Sector:
-            pass
+        elif prim._shape.type == Shape.Type.Capsule:
+            raise NotImplementedError
+
+        elif prim._shape.type == Shape.Type.Sector:
+            raise NotImplementedError
 
     @staticmethod
-    def rd_aabb(gui: GUI, cam: Camera, aabb: AABB):
-        assert gui != None and cam != None
+    def rd_aabb(gui: GUI, cam: Camera, aabb: AABB) -> None:
+        assert gui is not None and cam is not None
 
         top_left: Matrix = cam.world_to_screen(aabb.top_left)
         top_left.x /= cam.viewport.width
@@ -151,5 +164,5 @@ class Render():
         gui.rect([top_left.x, top_left.y], [bot_right.x, bot_right.y])
 
     @staticmethod
-    def rd_joint(gui, joint: Joint):
-        pass
+    def rd_joint(gui, joint: Joint) -> None:
+        raise NotImplementedError
