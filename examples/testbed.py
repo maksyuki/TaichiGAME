c9ff61ca8c1844e6e15fe1c189c8aa829ee25a2d
maksyuki <maksyuki@126.com>
2021-12-13 21:57:19 +0800

    test: modify testbed's framework

--- examples/testbed.py
+++ examples/testbed.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import sys
 import taichi as ti
 
@@ -6,8 +8,28 @@ import taichi as ti
 sys.path.append('../')
 
 from TaichiGAME.scene import Scene
+from TaichiGAME.math.matrix import Matrix
+from TaichiGAME.dynamics.body import Body
+import TaichiGAME.geometry.shape as sp
 
 ti.init(arch=ti.cpu)
 
 scene = Scene()
+
+data: List[Matrix] = [
+    Matrix([4.0, 4.0], 'vec'),
+    Matrix([3.0, 3.0], 'vec'),
+    Matrix([3.0, 1.0], 'vec'),
+    Matrix([6.0, 2.0], 'vec'),
+    Matrix([4.0, 4.0], 'vec')
+]
+
+poly: sp.Polygon = sp.Polygon()
+poly.vertices = data
+
+bd: Body = scene._world.create_body()
+bd.shape = poly
+# bd.rot = 3.14 / 3
+
+scene._dbvt.insert(bd)
 scene.show()
